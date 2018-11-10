dd2e6d9d00fa7b7487bd665f511a898dfb406ecb
Ankit agrawal <ankitagrawal620@gmail.com>
2018-11-10 21:04:37 +0530

    change db password

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     DB_HOST: "localhost",
     DB_NAME: "users",
     DB_USER: "root",
-    DB_PASSWORD: "abc123",
+    DB_PASSWORD: "ankit123",
     MAIL_USER: "abc1234@gmail.com",
     MAIL_PASSWORD: "abc@123",
     APP_URL: 'localhost:3000'
